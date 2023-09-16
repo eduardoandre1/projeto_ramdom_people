dff77330b1ce43431a8131257f69c14530f5148d
Eduardo André <eduardo_andre@id.uff.br>
2023-09-16 09:28:14 -0300

    git edit deploy

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -1,7 +1,13 @@
 import pg  from "pg";
 import dotenv from "dotenv";
+import { ClientConfig} from "pg";
 dotenv.config()
 const {Pool} = pg;
+const config:ClientConfig =
+{
+	connectionString: process.env.DATABASE_URL
+	ssl: process.env.NODE_ENV === "production"?true: false
+}
 const DB = new Pool({
 	host: "localhost",
 	port: 5432,

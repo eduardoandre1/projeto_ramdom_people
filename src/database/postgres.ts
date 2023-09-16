f74748c9936169471964f18b26583db483fc0eb8
Eduardo André <eduardo_andre@id.uff.br>
2023-09-16 09:28:49 -0300

    esqui a virgula

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -5,7 +5,7 @@ dotenv.config()
 const {Pool} = pg;
 const config:ClientConfig =
 {
-	connectionString: process.env.DATABASE_URL
+	connectionString: process.env.DATABASE_URL,
 	ssl: process.env.NODE_ENV === "production"?true: false
 }
 const DB = new Pool({

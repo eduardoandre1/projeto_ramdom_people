a8cc72d562962be3080511f0685a9f909f57d906
Eduardo André <eduardo_andre@id.uff.br>
2023-09-16 09:29:33 -0300

    agora foi

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -8,13 +8,6 @@ const config:ClientConfig =
 	connectionString: process.env.DATABASE_URL,
 	ssl: process.env.NODE_ENV === "production"?true: false
 }
-const DB = new Pool({
-	host: "localhost",
-	port: 5432,
-	user: "postgres",
-	password: "eduardo",
-	database: "random_people", 
-	ssl: process.env.NODE_ENV === "production"?true: false
-})
+const DB = new Pool(config)
 
 export default DB
